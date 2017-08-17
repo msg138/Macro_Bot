b832d0649c4c33797a7fa0f039dff7b311af0862
Michael George <streetbard16@hotmail.com>
2017-08-17 10:38:48 -0700

    Added simpletest.js to the gitignore so it is not uploaded.

--- bot.js
+++ bot.js
@@ -1,4 +1,12 @@
-// Get mouse coordinates, used for designing scripts.
+// Get mouse coordinates, used for designing scripts
+var test = undefined;
+var fs = require("fs");
+fs.exists(path, function (exists) {
+    if (exists) {
+        test = require(path);
+    }else
+        test = false;
+});
 
 var robot = require("robotjs");
 var keypress = require('keypress');
@@ -356,4 +364,8 @@ function runAction(cs){
 // Check if an argument was passed and load the script if so.
 if(process.argv.length > 2){
     loadScript(process.argv[2]);
+}
+
+if(test){
+    test.assertEquals(1, 2);
 }
